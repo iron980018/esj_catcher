fea204262848d5740ab0ddb46859299aa69cdfa8
iron980018 <67350577+iron980018@users.noreply.github.com>
2023-11-27 22:13:10 +0800

    Update ESJ爬蟲.py
    更新cookie輸入方式

--- ESJ爬蟲.py
+++ ESJ爬蟲.py
@@ -4,10 +4,10 @@ import time
 from bs4 import BeautifulSoup
 
 #func
-def catch_web(url,first_time,floder_name,floder_adress,episode):
+def catch_web(url,first_time,floder_name,floder_adress,episode,the_cookie):
     #以下為每次抓不同小說所需調整的
     headers = {
-        "cookie":"hidden=value; _ga=GA1.1.1654530658.1689834791; ws_ticrf=%2C1687995086%2C1683200328; msg_alert=10; ws_last_visit_code=46146743a4DGZcTKpLSlacfxdOXVA96hrMtnQROgMKJT3jNtK5RwwbRNhZ2rRx-NU5SSjGnEBn; ws_last_visit_post=649e84f3c16FvaQUTdIm40-U2j3aiyBCfmow98Fgrbyx2Co6OKpoQkpiXD1PIiKQ; ews_key=b699319da97zJm-mJrFuKJxKuBnZDT1gNJT7kGrFzphQPDYKMcnLBGODHDog; ews_token=9e1cb68c64ScC6a7r0vzYRW0V57CYug7v2gYvMtbR0TxwtzZWN6KSyuP_M6btY7bMavSuLg3yWZzR0HJskZ8UMbinKHTDNtreQbk_b; cf_clearance=QnoUimdn.cQ2gb1Dpuz693qIG5cqBNWBnJCSSwOl5CU-1692003316-0-1-5ee2aa38.4e275275.9e3f5d8-0.2.1692003316; _ga_6N355XR0Y6=GS1.1.1692003316.5.0.1692003316.0.0.0"
+        "cookie":the_cookie
     }
     #以上為每次抓不同小說所需調整的
 
@@ -56,11 +56,12 @@ def catch_web(url,first_time,floder_name,floder_adress,episode):
 
 print("ESJ爬蟲 by i9")
 print("此程式為教育使用，個人實作皆屬個人行為，本作者不負任何法律責任")
+cookie = input("請輸入你的cookie(請登入後再輸入):")
 start_url = input("請輸入起始網址(第1話):")
 
 first_time = 1
 floder_name = ""
 floder_adress = ""
 
-catch_web(start_url,first_time,floder_name,floder_adress,1)
+catch_web(start_url,first_time,floder_name,floder_adress,1,cookie)
 
